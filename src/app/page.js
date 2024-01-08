cb4e28f7a26847b5f89bfcca6cec72102e963f3b
MaximeLeBerre <70652404+MaximeLeBerre@users.noreply.github.com>
2024-01-08 07:09:00 +0100

    feat: banner

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,11 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Mon super blog</h1>
+        <div className={styles.hero}>
+            <h1>Je documente ma vie de dev</h1>
+            <div><span>Articles.</span><span>Pensées.</span><span>Expériences.</span></div>
+        </div>
+
     </main>
   )
 }
